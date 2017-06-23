04284416276517cbc79da6157d3ce0dbb0a4cd49
serfer89 <sergpavlov89@gmail.com>
2017-06-23 17:40:16 +0300

    Update jira.js

--- jira.js
+++ jira.js
@@ -1,15 +1,25 @@
 
+
+//console.log("/-"+login+", "+pass);
+
+var searchArgs;
+var session = 0;
+
+
+var token = function (login, pass, callback) {
 var Client = require('node-rest-client').Client;
+var temp_session = session;
+//this.tempt_session = session;
 
 
 client = new Client();
-// Provide user credentials, which will be used to log in to JIRA.
 
+// Provide user credentials, which will be used to log in to JIRA.
 
 var loginArgs = {
         data: {
-                "username": "s.pavlov@planeta-kino.com.ua",
-                "password": "Veronika87"
+                "username": login,
+                "password": pass
         },
         headers: {
                 "Content-Type": "application/json"
@@ -17,10 +27,13 @@ var loginArgs = {
 };
 
 
+
 client.post("https://planetakino.atlassian.net/rest/auth/1/session", loginArgs, function(data, response){
         if (response.statusCode == 200) {
-                console.log('succesfully logged in, session:', data.session);
-                var session = data.session;
+		
+                session = data.session;
+				temp_session = session;
+
                 // Get the session information and store it in a cookie in the header
                 var searchArgs = {
                         headers: {
@@ -34,17 +47,45 @@ client.post("https://planetakino.atlassian.net/rest/auth/1/session", loginArgs, 
                         }*/
                 };
 				// Make the request return the search results, passing the header information including the cookie.
-                client.post("http://planetakino.atlassian.net/rest/api/2/project", searchArgs, function(searchResult, response) {
+              /*  client.post("http://planetakino.atlassian.net/rest/api/2/project", searchArgs, function(searchResult, response) {
                         console.log('status code:', response.statusCode);
                         console.log('search result:', searchResult.length);
 			var i=searchResult.length;
    			searchResult.forEach(function(item, i, searchResult) { console.log( searchResult[i].name );});
                         console.log('search result:', searchResult[1].name);
-                });
+                });*/
+
+				
         }
         else {
                 throw "Login failed :(";
         }
+
+				callback(temp_session);
 });
 
 
+
+//console.log('succesfully logged in, session:', );
+
+
+}
+
+
+var login = "s.pavlov@planeta-kino.com.ua";
+var	pass = "Veronika87";
+
+token(login, pass, function(response) {
+console.log(response);
+return response;
+});
+
+
+
+function tester(){var t =1; return t;}
+
+
+module.exports.t = tester;
+module.exports.token = token;
+
+//console.log("orign"+tester(t));
