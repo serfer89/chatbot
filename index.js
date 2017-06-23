5f491ad371deb010391c60c0427d864675d75e44
serfer89 <sergpavlov89@gmail.com>
2017-06-24 01:20:46 +0300

    change auth string Base 64

--- index.js
+++ index.js
@@ -1,159 +1,162 @@
 'use strict';
 var login = "s.pavlov@planeta-kino.com.ua";
-var	pass = "Veronika87";
+var pass = "Veronika87";
+
+
+var auth_string = login+":"+pass;
+var base64 = require('base-64');
+auth_string = base64.encode(auth_string);
+auth_string = 'Basic '+auth_string;
+console.log(auth_string);
+
+var response;
+
 const TelegramBot = require('node-telegram-bot-api'),
-      request = require('request'),
-      fs = require('fs'),
-      token = '322237854:AAFh-FrdOO1As5LxIXx7rHEet6iiCRYvAHw',
-	bot = new TelegramBot(token, {polling:true});
+    request = require('request'),
+    fs = require('fs'),
+    token = '322237854:AAFh-FrdOO1As5LxIXx7rHEet6iiCRYvAHw',
+    bot = new TelegramBot(token, {
+        polling: true
+    });
 var auth = require("./jira.js");
-    var t = auth.t();
-	var session_token = auth.token(login, pass, function(response) {
+var t = auth.t();
 
-		console.log(response);
+var session_token = auth.token(login, pass, function(response) {
+    return response;
+});
 
-		return response;
+var get_project = auth.project(auth_string, function(response) {
+console.log(response[0].name);
+    return response;
+});
 
-		});
-
-
-
-
-
-
-	
-
- let stateSetName = false;
- let notes = [];
- let userName;
-    var main_options = {
-      reply_markup: JSON.stringify({
+let stateSetName = false;
+let notes = [];
+let userName;
+var main_options = {
+    reply_markup: JSON.stringify({
         inline_keyboard: [
-          [{ text: 'Погода', callback_data: 'погода' }],
-          [{ text: 'Курс', callback_data: 'курс' }],
-          [{ text: 'Нагадати', callback_data: 'нагадай' }],
-          [{ text: 'Інше', callback_data: 'інше' }]
+            [{
+                text: 'Погода',
+                callback_data: 'погода'
+            }],
+            [{
+                text: 'Курс',
+                callback_data: 'курс'
+            }],
+            [{
+                text: 'Нагадати',
+                callback_data: 'нагадай'
+            }],
+            [{
+                text: 'Інше',
+                callback_data: 'інше'
+            }]
         ]
-      })
+    })
+};
+
+function stock(id) {
+    request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5', function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            const data = JSON.parse(body);
+            data.forEach(function(value, index) {
+                bot.sendMessage(id, (`Валюта: ${value.ccy} | Код національної валюти: ${value.base_ccy} | Купівля: ${value.buy}`));
+
+            });
+            bot.sendMessage(id, 'поїхали далі', main_options);
+        }
+    })
+
+}
+
+function weather(id) {
+    request('http://api.openweathermap.org/data/2.5/forecast/daily?q=Kyiv&appid=d01946c71f847270df726ba46cc786b6&mode=json&units=metric&cnt=7', function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            const data = JSON.parse(body);
+
+            let data_html = "Температура сьогодні " + data.list[0].temp.day.toFixed(1) + ", " + data.list[0].weather[0].description + "http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png";
+
+            var data_html_t = "Температура на завтра " + data.list[1].temp.day.toFixed(1) + ", " + data.list[0].weather[0].description + "http://openweathermap.org/img/w/" + data.list[1].weather[0].icon + ".png";
+
+            bot.sendMessage(id, data_html),
+                bot.sendMessage(id, data_html_t),
+                bot.sendMessage(id, 'поїхали далі', main_options);
+
+        }
+    })
+}
+
+bot.on('callback_query', function(msg) {
+    const id = msg.from.id;
+    let answer = msg.data;
+    switch (answer) {
+        case 'погода':
+            weather(id);
+            break;
+        case 'курс':
+            stock(id);
+            break;
     };
-	
-	function stock(id){
-	request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5', function(error, 		response, body){
-	if(!error && response.statusCode === 200)
-		{
-		const data = JSON.parse(body);
-		data.forEach(function(value, index){
-			bot.sendMessage(id, (`Валюта: ${value.ccy} | Код національної валюти: ${value.base_ccy} | Купівля: ${value.buy}`));
-			
-			}); bot.sendMessage(id, 'поїхали далі', main_options);
-		}	
-	})
-				
-	}
-
-	function weather(id){
-	request('http://api.openweathermap.org/data/2.5/forecast/daily?q=Kyiv&appid=d01946c71f847270df726ba46cc786b6&mode=json&units=metric&cnt=7', function(error, 		response, body){
-	if(!error && response.statusCode === 200)
-		{
-		const data = JSON.parse(body);
-
-
-               
-let data_html = "Температура сьогодні "+data.list[0].temp.day.toFixed(1)+", "+data.list[0].weather[0].description+"http://openweathermap.org/img/w/"+data.list[0].weather[0].icon+".png";
-
- 
-
-var data_html_t = "Температура на завтра "+data.list[1].temp.day.toFixed(1)+", "+data.list[0].weather[0].description+"http://openweathermap.org/img/w/"+data.list[1].weather[0].icon+".png";
-
-			bot.sendMessage(id, data_html),
-			bot.sendMessage(id, data_html_t),
-			bot.sendMessage(id, 'поїхали далі', main_options);	
-			
-		}	
-	})
-	} 
-
-
-
-bot.on('callback_query', function (msg) { 
-	const id = msg.from.id;
-	let answer = msg.data;
-	switch (answer) {
-	case 'погода': weather(id); 
-	break;
-	case 'курс': stock(id); 
-	break;
-	};
-	
 
 });
 
-    bot.onText(/\/напомни (.+) в (.+)/, function (msg, match) {
-      var userId = msg.from.id;
-      var text = match[1];
-      var time = match[2];
+bot.onText(/\/напомни (.+) в (.+)/, function(msg, match) {
+    var userId = msg.from.id;
+    var text = match[1];
+    var time = match[2];
 
-      notes.push( { 'uid':userId, 'time':time, 'text':text } );
-
-      bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
-
-    setInterval(function(){
-        for (var i = 0; i < notes.length; i++){
-            var curDate = new Date().getHours() + ':' + new Date().getMinutes();
-                if ( notes[i]['time'] == curDate ) {
-                    bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: '+ notes[i]['text'] + ' сейчас.');
-                    notes.splice(i,1);
-                }
-            }
-    },1000);
-
-
+    notes.push({
+        'uid': userId,
+        'time': time,
+        'text': text
     });
 
+    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
 
-bot.onText(/\/setname/, function (msg, match) {
-  
-  var fromId = msg.from.id;
-  bot.sendMessage(fromId, "Введите имя").then(answer => {
-  console.log('He said ', answer.text); // He said all fine
+    setInterval(function() {
+        for (var i = 0; i < notes.length; i++) {
+            var curDate = new Date().getHours() + ':' + new Date().getMinutes();
+            if (notes[i]['time'] == curDate) {
+                bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: ' + notes[i]['text'] + ' сейчас.');
+                notes.splice(i, 1);
+            }
+        }
+    }, 1000);
+
 });
 
+bot.onText(/\/setname/, function(msg, match) {
 
-  /*stateSetName =true;
+    var fromId = msg.from.id;
+    bot.sendMessage(fromId, "Введите имя").then(answer => {
+        console.log('He said ', answer.text); // He said all fine
+    });
+
+    /*stateSetName =true;
  setTimeout(function(){   bot.sendMessage(fromId, "Отмена!"); stateSetName =false;},30000);*/
 });
 
+bot.on('message', function(msg) {
+    const id = msg.from.id,
+        _msgTxt = msg.text,
+        msgTxt = _msgTxt.toLowerCase();
 
+    if (msgTxt == '/start' || msgTxt == 'привет' || msgTxt == 'привіт') {
+        bot.sendMessage(id, 'Привіт, я бот Альфред! Я знаю яка зараз погода і курс валют', main_options);
+        //getname(msgTxt, id);
+    } else if (msgTxt == 'курс') {
+        stock(id);
+    } else if (msgTxt == 'погода') {
+        weather(id);
+    }
 
-
-bot.on('message', function(msg){ 
-	const id = msg.from.id,
-	_msgTxt = msg.text,
-	msgTxt = _msgTxt.toLowerCase();
-	
-	if(msgTxt == '/start' || msgTxt == 'привет' || msgTxt == 'привіт') {
-	bot.sendMessage(id, 'Привіт, я бот Альфред! Я знаю яка зараз погода і курс валют',  main_options);	
-	//getname(msgTxt, id);
-	}
-
-
-else if (msgTxt == 'курс') {stock(id);}
-
-
-
-
-else if (msgTxt == 'погода') {weather(id);}
-
-
-
-
-
-
-	function getname(msgTxt, id){
-	if (msgTxt != null)
-	{	
-	let username = msgTxt;
-		bot.sendMessage(id, 'What about your email?');	}
-	else {bot.sendMessage(id, 'Pls enter your name'); }
-	}
+    function getname(msgTxt, id) {
+        if (msgTxt != null) {
+            let username = msgTxt;
+            bot.sendMessage(id, 'What about your email?');
+        } else {
+            bot.sendMessage(id, 'Pls enter your name');
+        }
+    }
 });
--- jira.js
+++ jira.js
@@ -1,56 +1,46 @@
-
-
 //console.log("/-"+login+", "+pass);
 
 var searchArgs;
 var session = 0;
 var response;
+var token = function(login, pass, callback) {
+    var Client = require('node-rest-client').Client;
 
+    //this.tempt_session = session;
 
+    client = new Client();
 
+    // Provide user credentials, which will be used to log in to JIRA.
 
-var token = function (login, pass, callback) {
-var Client = require('node-rest-client').Client;
-
-//this.tempt_session = session;
-
-
-client = new Client();
-
-// Provide user credentials, which will be used to log in to JIRA.
-
-var loginArgs = {
+    var loginArgs = {
         data: {
-                "username": login,
-                "password": pass
+            "username": login,
+            "password": pass
         },
         headers: {
-                "Content-Type": "application/json"
-        } 
-};
+            "Content-Type": "application/json"
+        }
+    };
 
-
-
-client.post("https://planetakino.atlassian.net/rest/auth/1/session", loginArgs, function(data, response){
+    client.post("https://planetakino.atlassian.net/rest/auth/1/session", loginArgs, function(data, response) {
         if (response.statusCode == 200) {
-		
-                session = data.session;
 
+            session = data.session;
 
-                // Get the session information and store it in a cookie in the header
-                var searchArgs = {
-                        headers: {
-								// Set the cookie from the session information
-                                cookie: session.name + '=' + session.value,
-                                "Content-Type": "application/json"
-                        },
-                   /*     data: {
+            // Get the session information and store it in a cookie in the header
+            var searchArgs = {
+                headers: {
+                    // Set the cookie from the session information
+                    cookie: session.name + '=' + session.value,
+                    "Content-Type": "application/json"
+                },
+                /*     data: {
 								// Provide additional data for the JIRA search. You can modify the JQL to search for whatever you want.
                                 jql: "project='BOT'"
                         }*/
-                };
-				// Make the request return the search results, passing the header information including the cookie.
-              /*  client.post("http://planetakino.atlassian.net/rest/api/2/project", searchArgs, function(searchResult, response) {
+            };
+            // Make the request return the search results, passing the header information including the cookie.
+            /*  client.post("http://planetakino.atlassian.net/rest/api/2/project", searchArgs, function(searchResult, response) {
                         console.log('status code:', response.statusCode);
                         console.log('search result:', searchResult.length);
 			var i=searchResult.length;
@@ -58,31 +48,44 @@ client.post("https://planetakino.atlassian.net/rest/auth/1/session", loginArgs, 
                         console.log('search result:', searchResult[1].name);
                 });*/
 
-				
+        } else {
+            throw "Login failed :(";
         }
-        else {
-                throw "Login failed :(";
-        }
-
-				callback(session);
-});
-
-
-
-//console.log('succesfully logged in, session:', );
-
+        callback(session);
+    });
 
 }
 
+var project = function (auth_string, callback) {
 
+    var Client = require('node-rest-client').Client;
 
+    client = new Client();
+            var searchArgs = {
+                headers: {
+                    // Set the cookie from the session information
+                    "authorization": auth_string,
+                    "Content-Type": "application/json"
+                },};
 
+ client.get("http://planetakino.atlassian.net/rest/api/2/project",searchArgs, function(searchResult, response) {
+                        console.log('status code:', response.statusCode);
+                        console.log('search result:', searchResult.length);
+			//var i=searchResult.length;
+   			//searchResult.forEach(function(item, i, searchResult) { console.log( searchResult[i].name );});
+                        //console.log('search result:', searchResult[1].name);
+			callback(searchResult);
+                });
 
+}
 
-function tester(){var t =1; return t;}
-
+function tester() {
+    var t = 1;
+    return t;
+}
 
 module.exports.t = tester;
 module.exports.token = token;
+module.exports.project = project;
 
 //console.log("orign"+tester(t));
